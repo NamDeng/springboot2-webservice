42518d34764128288caba63fbbffdc9c5f39c9a7
nwh0526 <nwh0526>
2020-06-25 23:54:49 +0900

    JPA 출력 쿼리 설정

--- src/test/java/com/nwh/springboot/domain/PostsRepositoryTest.java
+++ src/test/java/com/nwh/springboot/domain/PostsRepositoryTest.java
@@ -10,7 +10,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-import static java.util.function.Predicate.isEqual;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
