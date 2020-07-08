d5e069ed7e355214065945f195ecd4e61d58bdc3
nwh0526 <nwh0526>
2020-07-08 23:27:44 +0900

    Controller, Service 구분 및 테스트 코드 작성

---
+++ src/main/java/com/nwh/springboot/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.nwh.springboot.web;
+
+
+import com.nwh.springboot.service.posts.PostsService;
+import com.nwh.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+    private final PostsService postService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postService.save(requestDto);
+    }
+}
